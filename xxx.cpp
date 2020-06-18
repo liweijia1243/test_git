d5bba228dd9b379b24c381f7ff66128ebdd16ae9
Weijia Li <lwj19931221@163.com>
2020-06-18 10:09:21 +0200

    学习使用markdown语句

--- xxx.cpp
+++ xxx.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout<<"hellosdsd git"<<endl;
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++)
+    {
+        cout<<"hellosdsd git"<<endl;
+    }
+
+    vector<int> vec(n,0);
+    for (auto iter=vec.begin(); iter!=vec.end();iter++)
+    {
+        *iter=iter-vec.begin();
+        cout<< *iter <<endl;
+    }
     return 0;
 }
